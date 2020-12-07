e6222dee738d17257e674cb0d3d439387bafa950
Jonathan Duran <jduran@Jonathans-MBP.fios-router.home>
2020-12-06 23:22:31 -0500

    files are now uploaded to S3 using hooks

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM apache/airflow:1.10.10
 
+RUN pip install upgrade pip && \
+    pip install --no-cache-dir boto3
+
 COPY ./data /opt/airflow/data
--- dags/us_covid_daily_report_pipeline.py
+++ dags/us_covid_daily_report_pipeline.py
@@ -1,16 +1,34 @@
+# Import Airflow specific modules
 from airflow import DAG 
-from airflow.operators.bash_operator import BashOperator 
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.sensors.http_sensor import HttpSensor
+from airflow.hooks import S3_hook
 
+# Import Python specific modules
 from datetime import datetime 
 import os
 
+# Set up connection to enable local files to be uploaded to S3
+def upload_file_to_S3_with_hook(filename, key, bucket_name):
+    """
+    Helper function to load a given file to an S3 bucket.
+
+    :param str filename: full path of file (including file extension) to be uploaded to S3
+    :param str key: filename to be inserted into the target S3 bucket
+    "param str bucket_name: name of S3 bucket 
+    """
+    hook = S3_hook.S3Hook('s3_connection')
+    hook.load_file(filename, key, bucket_name)
+
+# Initiate our dag definition
 dag = DAG(
     dag_id="us_covid_daily_report_pipeline",
-    start_date=datetime(2020, 11, 15),
+    start_date=datetime(2020, 12, 1),
     schedule_interval="@daily"
 )
 
+# Check if Covid api has data for a given date
 is_covid_api_available = HttpSensor(
     task_id="is_covid_api_available",
     method="GET",
@@ -21,6 +39,7 @@ is_covid_api_available = HttpSensor(
     timeout=20
 )
 
+# Download Covid US states data for a given date
 fetch_covid_us_data = BashOperator(
     task_id="fetch_covid_us_data",
     bash_command="curl -o /opt/airflow/data/{{ ds_nodash }}.json \
@@ -29,4 +48,17 @@ fetch_covid_us_data = BashOperator(
     dag=dag
 )
 
-is_covid_api_available >> fetch_covid_us_data
+# Upload Covid data to S3 using a helper function
+upload_file_to_s3 = PythonOperator(
+    task_id="upload_file_to_s3",
+    python_callable=upload_file_to_S3_with_hook,
+    op_kwargs={
+        "filename": "./data/{{ ds_nodash }}.json",
+        "key": "{{ ds_nodash }}.json",
+        "bucket_name": "datasets-jduran9987"
+    },
+    dag=dag
+)
+
+# Setting our dependencies
+is_covid_api_available >> fetch_covid_us_data >> upload_file_to_s3
